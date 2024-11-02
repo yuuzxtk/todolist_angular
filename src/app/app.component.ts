614b64f7c02498e05c0975500c90a3cb0e8f2bb9
Bowie <fhtumiatejunior@outlook.com>
2024-11-02 02:26:51 -0300

    teste todolist

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,51 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { Task } from './core/models/task.model';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'projeto1';
+  title: string = 'todolist';
+  taskname:string = 'novo'
+  welcomeMessage: string = 'Welcome to my first Angular App!';
+  showList: boolean = true;
+
+  tasks: Task[] = [
+    {title: 'Arrumar o quarto', done: false},
+    {title: 'Estudar para a facul', done: false},
+    {title: 'Ler emails', done: false},
+    {title: 'Lavar a roupa', done: false}
+
+  ];
+
+
+
+
+
+
+  helloworld(){
+    console.log('Hello World!');
+  }
+
+  updateTitle(title: string){
+    this.title = title;
+  }
+  newTask(taskname: string){
+    this.taskname = taskname;
+  }
+
+  addTask(){
+    this.tasks.push({title: this.taskname, done: false});
+  }
+
+  removeTask(index: number){
+    this.tasks.splice(index, 1);
+  }
 }
